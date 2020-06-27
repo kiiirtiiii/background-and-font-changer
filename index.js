2eebaec87ed01456e76fd01f8f444c03a033293b
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-06-27 23:02:53 +0100

    Update index.js
    refactored to be shorter and more concise. See if you can work out how I've done this.

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-function changeBackground(color) {
-	return function() {
-		document.getElementById("background").style.background = color;
-	};
-}
+const colors = {
+	blue: "#62a0f0",
+ 	grey: "lightgrey",
+  green: "#5bad2b",
+  red: "#db2b14",
+  yellow: "#fae92f",
+  white: "white",
+  black: "black"	
+};
 
-function changeText(color) {
-	return function() {
-		document.getElementById("sample-text").style.color = color;
-	};
-}
-
-var blue = changeBackground("#62a0f0");
-var grey = changeBackground("lightgrey");
-var green = changeBackground("#5bad2b");
-var red = changeBackground("#db2b14");
-var yellow = changeBackground("#fae92f");
-var white = changeBackground("white");
-var black = changeBackground("black");
-
-var textblue = changeText("blue");
-var textgrey = changeText("grey");
-var textgreen = changeText("green");
-var textred = changeText("red");
-var textyellow = changeText("yellow");
-var textwhite = changeText("white");
-var textblack = changeText("black");
-
-document.getElementById("bg-blue").onclick = blue;
-document.getElementById("bg-grey").onclick = grey;
-document.getElementById("bg-green").onclick = green;
-document.getElementById("bg-red").onclick = red;
-document.getElementById("bg-yellow").onclick = yellow;
-document.getElementById("bg-white").onclick = white;
-document.getElementById("bg-black").onclick = black;
-
-document.getElementById("bg-textblue").onclick = textblue;
-document.getElementById("bg-textgrey").onclick = textgrey;
-document.getElementById("bg-textgreen").onclick = textgreen;
-document.getElementById("bg-textred").onclick = textred;
-document.getElementById("bg-textyellow").onclick = textyellow;
-document.getElementById("bg-textwhite").onclick = textwhite;
-document.getElementById("bg-textblack").onclick = textblack;
+Object.keys(colors).forEach(color => {
+	const backgroundButton = document.getElementById(`bg-${color}`);
+	const textButton = document.getElementById(`bg-text${color}`);
+	backgroundButton.addEventListener('click', () => document.getElementById("background").style.background = colors[color]);
+	textButton.addEventListener('click', () => document.getElementById("sample-text").style.color = colors[color]);
+});
